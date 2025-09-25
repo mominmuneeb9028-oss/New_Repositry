c7e52de4bc78cebf1bc44777d621d30253b4b460
mominmuneeb9028-oss <mominmuneeb9028@gmail.com>
2025-09-25 07:48:06 +0530

    Arithmatic Operation in php is added

---
+++ arithmaticoperation.php
@@ -0,0 +1,23 @@
+<?php
+
+// Declare two numbers
+$a = 10;
+$b = 5;
+
+// Perform arithmetic operations
+$sum = $a + $b;
+$diff = $a - $b;
+$product = $a * $b;
+$quotient = $a / $b;
+$modulus = $a % $b;
+
+// Display results
+echo "a = $a<br>";
+echo "b = $b<br><br>";
+
+echo "Addition (a + b) = $sum<br>";
+echo "Subtraction (a - b) = $diff<br>";
+echo "Multiplication (a * b) = $product<br>";
+echo "Division (a / b) = $quotient<br>";
+echo "Modulus (a % b) = $modulus<br>";
+?>

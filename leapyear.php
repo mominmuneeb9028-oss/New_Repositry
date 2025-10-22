ef5a3a5a267c0e00789df06b3f42d97c795d7051
mominmuneeb9028-oss <mominmuneeb9028@gmail.com>
2025-10-22 07:18:03 +0530

    leap year or Not

---
+++ leapyear.php
@@ -0,0 +1,9 @@
+<?php
+$year = 2024; // You can change the year here
+
+if (($year % 4 == 0 && $year % 100 != 0) || ($year % 400 == 0)) {
+    echo "$year is a leap year.";
+} else {
+    echo "$year is not a leap year.";
+}
+?>

9fcac2ad4da1826d4750d911fc13a7948e3a852a
mominmuneeb9028-oss <mominmuneeb9028@gmail.com>
2025-11-04 21:54:58 +0530

    Quotient And Reminder

---
+++ quotient&reminder.php
@@ -0,0 +1,15 @@
+<?php
+$num1 = 20;
+$num2 = 3;
+
+//calculate quotient and remineder
+
+$quotient = intdiv($num1,$num2);
+$reminder = $num1 % $num2;
+
+//Display Result
+echo "Number 1: $num1 <br><br>";
+echo "Number 2: $num2 <br><br>";
+echo "Quotient: $quotient <br><br>";
+echo "Reminder: $reminder";
+?>
